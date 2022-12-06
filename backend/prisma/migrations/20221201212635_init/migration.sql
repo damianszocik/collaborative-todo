9f1d02b08bef1ba749d6a25018dfdb5b5ad3fa2b
Damian Szocik <damian.szocik@gmail.com>
2022-12-06 21:58:20 +0100

    setup prisma with Todo model

---
+++ backend/prisma/migrations/20221201212635_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
